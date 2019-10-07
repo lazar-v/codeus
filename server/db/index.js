ce4ac15320e67f2e8c0f744b4f6d0dfe0b2c64d4
lazar <lazarv@elfak.rs>
2019-10-07 14:16:12 +0200

    asd

--- server/db/index.js
+++ server/db/index.js
@@ -3,8 +3,6 @@ const Sequelize = require('sequelize');
 sequelize = new Sequelize(`postgres://wycepjsqpnynto:05728740ec601dfedfd057a0a858610495bd10e4c8837368af2f651e005dfb11@ec2-54-225-205-79.compute-1.amazonaws.com:5432/d73m52c70lcqnl?ssl=true`, {
     dialect: 'postgres',
     protocol: 'postgres',
-    port: match[4],
-    host: match[3],
     logging: true
 });
 
