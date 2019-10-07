7ddd1970d33b2464b89d0e8bc8b7d227b2efc8c8
lazar <lazarv@elfak.rs>
2019-10-07 14:10:31 +0200

    Heroku is to blame, not me

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,15 @@
 const Sequelize = require('sequelize');
 
-if(process.env.DATABASE_URL) {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        port: match[4],
-        host: match[3],
-        logging: true
-    });
-}
-else {
-    sequelize = new Sequelize('postgres://codeus:codeus@localhost:5432/codeus');
-}
+sequelize = new Sequelize(`postgres://wycepjsqpnynto:05728740ec601dfedfd057a0a858610495bd10e4c8837368af2f651e005dfb11@ec2-54-225-205-79.compute-1.amazonaws.com:5432/d73m52c70lcqnl?ssl=true`, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    port: match[4],
+    host: match[3],
+    logging: true
+});
+
+// sequelize = new Sequelize('postgres://codeus:codeus@localhost:5432/codeus');
+
 
 module.exports = sequelize;
 
--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ sequelize.authenticate()
     .then( () => console.log('Database connected..'))
     .catch( err => console.log(`Error: ${err}`));
 
-app.listen(process.env.PORT || 3000, () =>
-    console.log(`Server is running on port ${process.env.PORT || 3000}`));
+sequelize.sync()
+    .then( () =>
+        app.listen(process.env.PORT || 3000, () =>
+            console.log(`Server is running on port ${process.env.PORT || 3000}`)));
 
